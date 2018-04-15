35f00d8ccc6793122136cd30cc2ff58402c5d039
Franktian <tianyawen201209@hotmail.com>
2018-04-15 13:29:49 -0400

    Added event form

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+import { Button, Popover, PopoverBody } from 'reactstrap';
 
 import '../Days/Days.css';
 
@@ -22,13 +22,31 @@ class Day extends Component {
     render() {
         const activeDay = this.props.data.isActive ? " active" : "";
         const isToday = this.props.data.isToday ? " today" : "";
-        const isoString = this.props.data.toISOString();
 
         return (
             <div className={"days-item" + activeDay + isToday}>
                 <div className="text" id={'Popover-' + this.props.id} onClick={this.toggle}>{this.props.data.getDate()}</div>
                 <Popover className="pop-over-container" placement="bottom" isOpen={this.state.popoverOpen} target={'Popover-' + this.props.id} toggle={this.toggle}>
-                    <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
+                    <PopoverBody>
+                        <div className="input-row">
+                            <div className="row-item-1">
+                                <input size="10" placeholder="Hour"/>
+                            </div>
+                            <div className="row-item-1 right">
+                                <input size="10" placeholder="Minute"/>
+                            </div>
+                        </div>
+
+                        <div className="input-row">
+                            <div className="row-item-1">
+                                <input size="23"  placeholder="Event Name"/>
+                            </div>
+                        </div>
+
+                        <div className="btn-container">
+                            <Button color="secondary">Save</Button>
+                        </div>
+                    </PopoverBody>
                 </Popover>
             </div>
         );
